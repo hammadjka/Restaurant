322803899c622b2009cc76696b7ee21daf3e02a9
hammadjka <98783499+hammadjka@users.noreply.github.com>
2024-02-08 06:35:24 -0500

    added default page on refresh

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const about = document.querySelector("#about");
     const menu = document.querySelector("#menu");
     const contact = document.querySelector("#contact");
-
     about.addEventListener('click', loadAboutUs);
+    about.click();
     menu.addEventListener('click', loadMenu);
     contact.addEventListener('click', loadContact)
 });
